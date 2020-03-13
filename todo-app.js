15ffa7f2f0a7d4156fe7db9814982e1642ca9e1a
salmanAltaf101 <sllujaan44@gmail.com>
2020-03-13 17:05:18 +0500

    made some containers

--- todo-app.js
+++ todo-app.js
@@ -1,1 +1,29 @@
-var item = 10;
+
+
+document.addEventListener('click', (event)=> {
+    console.log(event.target)
+    var id = event.target.getAttribute("id")
+    console.log("id = ", id)
+})
+
+
+
+
+
+/*
+
+var remove = document.getElementsByClassName("fa-trash-alt")
+console.log(remove)
+console.log(remove.length)
+
+
+for(i=0; i<remove.length; i++){
+    console.log(remove[i])
+    remove[i].addEventListener('click', onRemove)
+}
+
+function onRemove(event){
+    console.log(event.target)
+}
+
+*/
